def87adf1a82ec27eab2fdaa9bc3a3749cf30e3d
Pauline_Awuor <paulineawuor.creative@gmail.com>
2024-06-22 10:09:49 +0300

    first commit

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mainController = require('../controllers');
+const authRouter = require('./auth');
+
+const mainRouter = express.Router();
+
+mainRouter.get('/', mainController.main);
+
+mainRouter.use('/auth', authRouter);
+
+module.exports = mainRouter;
+
+app.listen(3000)
