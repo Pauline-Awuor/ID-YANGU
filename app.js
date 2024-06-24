ac51659ae4b790293dea7dfa2ce8a5cf2f3d4e24
omuondzi <omondih401@gmail.com>
2024-06-24 19:30:05 +0300

    hillary,s commit

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const mainRouter = require('./routes');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const mainRouter = require("./routes");
 
 dotenv.config();
 
@@ -11,24 +11,34 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept, Origin, X-Requested-With');
-    next();
-})
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Accept, Origin, X-Requested-With"
+  );
+  next();
+});
 
 const mongoUrl = process.env.MONGO_URL;
 
-app.use(mainRouter)
+app.use(mainRouter);
 
 app.use((req, res) => {
-    res.status(404).json({ message: 'Page not found' })
-})
+  res.status(404).json({ message: "Page not found" });
+});
 
-mongoose.set('strictQuery', false).connect(mongoUrl).then(() => {
+mongoose
+  .set("strictQuery", false)
+  .connect(mongoUrl)
+  .then(() => {
     app.listen(3000, () => {
-        console.log('Server is running on port 3000');
-    })
-}).catch((err) => {
-    console.log(err)
-})
+      console.log(`Server is running on port 3000`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
