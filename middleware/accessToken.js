269907e69e99aa0a3cd3febb1d1c6d6924c88714
Pauline_Awuor <paulineawuor.creative@gmail.com>
2024-07-11 23:16:13 +0300

    fix: update token function

--- middleware/accessToken.js
+++ middleware/accessToken.js
@@ -1,16 +1,29 @@
-const jwt= require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const User = require("../models/user");
-const HttpError= require("../models/http-error");
+const HttpError = require("../models/http-error");
 
-
-async function accessToken(
-  req,
-  res,
-  next
-) {
+async function accessToken(req, res, next) {
   try {
-    const token = req?.headers?.authorization?.split(" ")[1];
-    if (!token)
+// Log headers
+
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return next(
+        new HttpError(
+          "Access token error",
+          "Authorization header is missing",
+          401
+        )
+      );
+    }
+
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+      return next(new HttpError("Access token error", "Token is missing", 401));
+    }
+
+    const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
+    if (!decodedToken || !decodedToken._id) {
       return next(
         new HttpError(
           "Access token error",
@@ -18,35 +31,20 @@ async function accessToken(
           401
         )
       );
-      const decodedToken = jwt.verify(
-        token,
-      process.env.SECRET_KEY
-      );
-    const id = decodedToken?._id.trim();
+    }
 
-    const foundProfile = (await User.findById(id, {
-      password: 0,
-    })) ;
-
+    const userId = decodedToken._id.trim();
+    const foundProfile = await User.findById(userId, { password: 0 });
     if (!foundProfile) {
       return next(new HttpError("Error", "User not found", 404));
     }
 
-    if (!foundProfile)
-      return next(
-        new HttpError(
-          "Access token error",
-          "Invalid or expired access token",
-          401
-        )
-      );
-    req.user = { ...foundProfile["_doc"] };
-   
+    req.user = { ...foundProfile._doc };
     next();
   } catch (err) {
-    console.log(err);
+    console.error("Access token validation failed:", err);
     return next(new HttpError("Access token validation failed", null, 401));
   }
 }
 
-module.exports = accessToken
+module.exports = accessToken;
