b56ad0fe30e3687bc84ab3200d4f513fb4d19498
Pauline_Awuor <paulineawuor.creative@gmail.com>
2024-07-11 22:22:47 +0300

    remove

--- env.js
+++
@@ -1,7 +0,0 @@
-SECRET_KEY=grey20020
-MONGO_URL=mongodb+srv://Apoh:Grey2002@pauline.o5xfim7.mongodb.net/Fynd?retryWrites=true&w=majority&appName=Pauline
-SMTP_EMAIL= noreply.fynd@gmail.com
-SMTP_PASSWORD= buxsqkmemfzgkpal
-SMTP_HOST= smtp.gmail.com
-SMTP_PORT= 465
-CLIENT_URL=https://platform-fynd.web.app
