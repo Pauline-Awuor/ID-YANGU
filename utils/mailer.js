60b9902cc8fdc066de9ebe1d16c5516bcfde7f4c
Pauline_Awuor <paulineawuor.creative@gmail.com>
2024-07-11 23:13:15 +0300

    fix: update the email message

--- utils/mailer.js
+++ utils/mailer.js
@@ -30,7 +30,7 @@ async function sendIdNotFoundNotification(email, idNumber) {
   const subject = "ID Search Notification";
   const html = `
     <p>Dear User,</p>
-    <p>Thank you for using our ID Finder service. We regret to inform you that we could not find your ID card with the number <strong>${idNumber}</strong> at this time.</p>
+    <p>Thank you for using our service. We regret to inform you that we could not find your ID card with the number <strong>${idNumber}</strong> at this time.</p>
     <p>We will notify you as soon as your ID card is found.</p>
     <p>Thank you for your patience.</p>
     <p>Best regards,<br>FYND Team</p>
