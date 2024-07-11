5be7d1fae36e1de70e5dd9af6d7f9eac09e5d922
Pauline_Awuor <paulineawuor.creative@gmail.com>
2024-07-11 23:06:36 +0300

    fix: remove access token

--- routes/idDetail.js
+++ routes/idDetail.js
@@ -9,30 +9,13 @@ router.post("/", accessToken, idController.createId);
 
 router.patch("/:id", accessToken, idController.updateId);
 
-router.get("/", accessToken, idController.getAllIds);
+router.get("/", idController.getAllIds);
 
 router.get("/:id", accessToken, idController.getId);
 
 router.delete("/:id", accessToken, idController.deleteId);
 
 // Search endpoint
-router.get("/search", accessToken, async (req, res, next) => {
-  try {
-    const { idNumber } = req.query;
-    const foundId = await idController.findId(idNumber);
-
-    if (foundId) {
-      return res.status(200).json(foundId);
-    } else {
-      const existingRequest = await IdRequest.findOne({ email: req.user.email, idNumber });
-      if (!existingRequest) {
-        await IdRequest.create({ email: req.user.email, idNumber });
-      }
-      return res.status(404).json({ message: 'ID not found. You will be notified when it is found.' });
-    }
-  } catch (err) {
-    next(err);
-  }
-});
+router.get("/search", accessToken, idController.searchId);
 
 module.exports = router;
